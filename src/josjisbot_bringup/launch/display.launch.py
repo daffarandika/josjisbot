85357956b051f4c5322fbe24057e5d9bf62f66d1
Daffa Randika <daffa.randika.3@gmail.com>
2025-09-21 22:53:10 +0700

    chore: added the correct launch file

--- src/josjisbot_bringup/launch/display.launch.py
+++ src/josjisbot_bringup/launch/display.launch.py
@@ -1,55 +1,32 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, Command
 
 
 def generate_launch_description():
-    pkg_name = 'josjisbot_description'
+    is_robot_centric = LaunchConfiguration("is_robot_centric")
 
-    urdf_file = os.path.join(
-        get_package_share_directory(pkg_name),
-        'urdf',
-        'josjisbot.urdf.xacro'
+    robot_centric_controller = Node(
+        package="josjisbot_controller",
+        executable="robot_centric_controller",
+        output="screen",
+        condition=IfCondition(is_robot_centric),
     )
 
-    robot_description_content = Command(['xacro ', urdf_file])
-    robot_description = {'robot_description': robot_description_content}
-
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
-    )
-
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        output='screen',
-        parameters=[{'use_sim_time': False}]
-    )
-
-    # Node RViz2
-    rviz_config_dir = os.path.join(
-        get_package_share_directory(pkg_name),
-        'rviz',
-        'display.rviz'
-    )
-
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config_dir],
-        parameters=[{'use_sim_time': False}]
+    world_centric_controller = Node(
+        package="josjisbot_controller",
+        executable="world_centric_controller",
+        output="screen",
+        condition=UnlessCondition(is_robot_centric),
     )
 
     return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz_node,
+        DeclareLaunchArgument(
+            "is_robot_centric",
+            default_value="false",
+        ),
+        robot_centric_controller,
+        world_centric_controller,
     ])
